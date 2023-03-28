f5ec4bf285b205dcab91e714178e647af753b857
Jin <hellothisisjin@gmail.com>
2023-03-28 16:59:07 +0900

    feat : Add JS exception handling

--- FE/13_Async/10_AsyncAwait.js
+++ FE/13_Async/10_AsyncAwait.js
@@ -7,23 +7,43 @@
     - async, await should be a pair
 */
 
+/* JS Exception handling
+    try {
+        // if error occurs move to catch block
+    } catch (error) {
+        // exception handling
+    }
+    // if there is no error on try {}, catch {} does not execute
+    // if there is error on try {}, try {} stop running -> move straight to catch {}
+*/
+
 // Print elements of fruits array after 1s
 function fetchFruits() {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
       const fruits = ["🍉", "🥝", "🍋"];
       resolve(fruits);
+      //   reject(new Error("Undefined error occurs! Cannot get itmes."));
     }, 1000);
   });
 }
 
 // Using Promise
-fetchFruits().then(function (fruits) {
-  console.log(fruits);
-});
+fetchFruits()
+  .then(function (fruits) {
+    console.log(fruits);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 // Using JS keyword async, await
 async function printItems() {
-  const fruits = await fetchFruits();
-  console.log(fruits);
+  try {
+    const fruits = await fetchFruits();
+    console.log(fruits);
+  } catch (error) {
+    console.log(error);
+  }
 }
+printItems();
