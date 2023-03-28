8fe493fccc7ab961b6f980bf7f52a0ce56a8665e
Jin <hellothisisjin@gmail.com>
2023-03-28 17:14:51 +0900

    feat : Add JS async await practice

---
+++ FE/13_Async/11_BuyDrink_AsyncAwait.js
@@ -0,0 +1,30 @@
+function goMart() {
+  console.log("Choose what to buy");
+}
+
+function pickDrink() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log("Choose!");
+      product = "Grape juice";
+      price = 2000;
+
+      resolve();
+    }, 3000);
+  });
+}
+
+function pay() {
+  console.log(`Product Name: ${product}, Price: ${price}`);
+}
+
+async function exec() {
+  goMart(); // 1
+  await pickDrink(); // 2, wait for the pickDrink() to finish
+  pay(); //3
+}
+
+let product; // undefined
+let price; // undefined
+
+exec();
