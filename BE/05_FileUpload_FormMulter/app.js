ca09550e212744307158653a75159f4c2a350b11
Jin <hellothisisjin@gmail.com>
2023-04-01 10:02:53 +0900

    feat : Add multer options

--- BE/05_FileUpload_FormMulter/app.js
+++ BE/05_FileUpload_FormMulter/app.js
@@ -5,9 +5,35 @@ const PORT = 8000;
 // Using multer
 const multer = require("multer");
 const path = require("path"); // Built-in modules(no need to import), make easier to set file path
-const upload = multer({
-  dest: "uploads/",
-}); /// Set destination directory path where files will be uploaded in the server from clients
+// const upload = multer({
+//   dest: "uploads/",
+// }); // Set destination directory path where files will be uploaded in the server from clients
+const uploadDetail = multer({
+  storage: multer.diskStorage({
+    // Determine within which folder the uploaded files should be stored
+    destination(req, file, callback) {
+      callback(null, "uploads/");
+    },
+    // Determine what the file should be named inside the folder
+    filename(req, file, callback) {
+      const extension = path.extname(file.originalname); // Extract extension from callback parameter, file
+      /* A server commnicates with several clients, 
+        so the file names can be the same 
+        add current time to remove duplicate names */
+      // path.basename() returns without file extension
+      // Date.now() returns milliseconds from 1970 Jan 1, 0 minute 0 second
+      callback(
+        null,
+        path.basename(file.originalname, extension) + Date.now() + extension
+      );
+    },
+  }),
+  limits: { fileSzie: 5 * 1024 * 1024 }, // Set file size limit in 5MB
+  /* cf. file size:
+    5 * (2 ^ 10) = 5KB
+    5 * (2 ^ 10) * (2 ^ 10) = 5MB
+  */
+});
 
 app.set("view engine", "ejs");
 app.use("/views", express.static(__dirname + "/views"));
@@ -22,9 +48,10 @@ app.get("/", (req, res) => {
 
 /* single("[FILE_NAME]") : Upload one file */
 // <input type="file" name="[FILE_NAME]" />
-app.post("/upload", upload.single("userfile"), (req, res) => {
-  console.log("req.file: "); // Information of uploaded file
-  console.log(req.file);
+// app.post("/upload", upload.single("userfile"), (req, res) => {
+app.post("/upload", uploadDetail.single("userfile"), (req, res) => {
+  console.log(req.file); // Information of uploaded file
+  /* req.file */
   // {
   //   fieldname: 'userfile', // name of input type="file"
   //   originalname: 'posts-11.jpg',
@@ -36,8 +63,7 @@ app.post("/upload", upload.single("userfile"), (req, res) => {
   //   size: 175830
   // }
 
-  console.log("req.body: "); // Infromation in the form
-  console.log(req.body);
+  console.log(req.body); // Infromation in the form
 
   res.send("Upload Succeed!");
 });
