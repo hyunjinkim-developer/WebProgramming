145cda0276a4fa17860606b9e55c989d4c22ae30
Jin <hellothisisjin@gmail.com>
2023-04-06 20:12:00 +0900

    feat : Connect Node.js with MySQL, Create, Read, Update, Delete data

--- BE/09_ConnectMySQL_withMVC/controller/Cvisitor.js
+++ BE/09_ConnectMySQL_withMVC/controller/Cvisitor.js
@@ -14,18 +14,48 @@ exports.getVisitors = (req, res) => {
 
   /* After connecting to MySQL DB */
   Visitor.getVisitors((result) => {
-    console.log("Cvisitor.js >>", result);
+    // console.log("Cvisitor.js >>", result);
     res.render("visitor", { data: result });
   });
 };
 
 // POST /visitor/write
 exports.postVisitor = (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
   // result(from model, inserted data) comes in as rows.insertId
   Visitor.postVisitor(req.body, (result) => {
-    console.log("Cvisitor.js >>", result);
+    // console.log("Cvisitor.js >>", result);
     res.send({ id: result, name: req.body.name, comment: req.body.comment });
+  });
+};
+
+// GET /visitor/get
+exports.getVisitor = (req, res) => {
+  console.log(req.query); // { id: n }
+
+  Visitor.getVisitor(req.query.id, (result) => {
+    console.log(result); // rows[0] from model callback
+    res.send(result);
+  });
+};
+
+// PATCH /visitor/edit
+exports.patchVisitor = (req, res) => {
+  console.log(req.query);
+
+  // Send all request data to model, becuase don't know which data will change
+  Visitor.patchVisitor(req.body, () => {
+    res.send("Edit Succeed!");
+  });
+};
+
+// DELTE /visitor/delete
+exports.deleteVisitor = (req, res) => {
+  console.log(req.body); // { id: n }
+
+  Visitor.deleteVisitor(req.body.id, (result) => {
+    // console.log("Cvisitor.js >>", result);
+    res.send({ id: req.body.id });
   });
 };
--- BE/09_ConnectMySQL_withMVC/model/Visitor.js
+++ BE/09_ConnectMySQL_withMVC/model/Visitor.js
@@ -28,14 +28,14 @@ exports.getVisitors = (callback) => {
       throw error;
     }
 
-    console.log("Visitor.js >>", rows);
+    // console.log("Visitor.js >>", rows);
     callback(rows);
   });
 };
 
 exports.postVisitor = (data, callback) => {
   // Data from controller, form sent by client (req.bdoy)
-  console.log(data);
+  // console.log(data);
 
   const sql = `INSERT INTO VISITOR(name, comment) VALUES("${data.name}", "${data.comment}")`;
   conn.query(sql, (err, rows) => {
@@ -44,5 +44,47 @@ exports.postVisitor = (data, callback) => {
     }
     console.log("Visitor.js >>", rows.insertId);
     callback(rows.insertId);
+  });
+};
+
+exports.getVisitor = (id, callback) => {
+  console.log(id);
+
+  const sql = `SELECT * FROM visitor WHERE id = ${id}`;
+  conn.query(sql, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("Visitor.js >>", rows); // rows [ {} ]
+    callback(rows[0]);
+  });
+};
+
+exports.patchVisitor = (data, callback) => {
+  console.log(data); // req.body from controller
+
+  const sql = `UPDATE visitor SET name = "${data.name}", comment = "${data.comment}" WHERE id = ${data.id}`;
+  conn.query(sql, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log("patchVisitor Visitor.js >>", rows);
+    callback();
+  });
+};
+
+exports.deleteVisitor = (id, callback) => {
+  // console.log(id); // id from controller, req.body.id
+
+  const sql = `DELETE FROM visitor WHERE id = ${id}`;
+  conn.query(sql, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    // console.log("Visitor.js >>", rows);
+    callback(true);
   });
 };
