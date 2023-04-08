40ba1ffe1ad658115fdeb88dbb2f57273310a999
Jin <hellothisisjin@gmail.com>
2023-04-08 16:10:53 +0900

    feat: Using sequelize

--- BE/10_Sequelize_withMVC/controller/Cvisitor.js
+++ BE/10_Sequelize_withMVC/controller/Cvisitor.js
@@ -1,61 +1,156 @@
-const Visitor = require("../model/Visitor");
+// Before using Sequelize
+// const Visitor = require("../model/Visitor");
+
+// After using Sequelize
+// Ref: https://sequelize.org/docs/v7/core-concepts/model-querying-basics/
+// models = db oject from models/index.js
+const models = require("../models"); // If the name of .js not specified, automatically find index.js
+// console.log(models);
 
 // GET /
 exports.main = (req, res) => {
   res.render("index");
 };
 
-// GET /visitor
-// controller's getVisitors()
-exports.getVisitors = (req, res) => {
-  // /* Before connecting to MySQL */
-  // console.log(Visitor.getVisitors()); // model's getVisiors()
-  // res.render("visitor", { data: Visitor.getVisitors() });
+// GET /visitor //////////////////////////////////////////
+// /* .then() */
+// exports.getVisitors = (req, res) => {
+//   // /* Before using Sequelize */
+//   // Visitor.getVisitors((result) => {
+//   //   res.render("visitor", { data: result });
+//   // }); // SELECT * FROM visitor;
 
-  /* After connecting to MySQL DB */
-  Visitor.getVisitors((result) => {
-    // console.log("Cvisitor.js >>", result);
-    res.render("visitor", { data: result });
-  });
+//   /* After using Sequelize */
+//   models.Visitor.findAll().then((result) => {
+//     console.log("findAll >>", result); // [ {}, {}, {} ]
+//     res.render("visitor", { data: result });
+//   });
+// };
+
+/* async, await */
+exports.getVisitors = async (req, res) => {
+  const result = await models.Visitor.findAll();
+  // console.log("findAll >>", result); // [ {}, {}, {} ]
+  res.render("visitor", { data: result });
 };
 
-// POST /visitor/write
-exports.postVisitor = (req, res) => {
-  // console.log(req.body);
+// POST /visitor/write ///////////////////////////////////
+// /* .then() */
+// exports.postVisitor = async (req, res) => {
+//   // /* Before using Sequelize */
+//   // Visitor.postVisitor(req.body, (result) => {
+//   //   res.send({ id: result, name: req.body.name, comment: req.body.comment });
+//   // }); // INSERT INTO VISITOR (name, comment) VALUES ("${data.name}", "${data.comment}");
 
-  // result(from model, inserted data) comes in as rows.insertId
-  Visitor.postVisitor(req.body, (result) => {
-    // console.log("Cvisitor.js >>", result);
-    res.send({ id: result, name: req.body.name, comment: req.body.comment });
+//   models.Visitor.create({
+//     name: req.body.name,
+//     comment: req.body.comment,
+//   }).then((result) => {
+//     console.log("create >>", result);
+//     res.send(result);
+//   });
+// };
+
+/* async, await */
+exports.postVisitor = async (req, res) => {
+  /* After using Sequelize */
+  const result = await models.Visitor.create({
+    name: req.body.name,
+    comment: req.body.comment,
   });
+  // console.log("create >>", result);
+  res.send(result);
 };
 
-// GET /visitor/get
-exports.getVisitor = (req, res) => {
-  console.log(req.query); // { id: n }
+// GET /visitor/get //////////////////////////////////////
+// /* .then() */
+// exports.getVisitor = (req, res) => {
+//   // /* Before using Sequelize */
+//   // Visitor.getVisitor(req.query.id, (result) => {
+//   //   res.send(result);
+//   // }); // SELECT * FROM visitor WHERE id = ${id};
 
-  Visitor.getVisitor(req.query.id, (result) => {
-    console.log(result); // rows[0] from model callback
-    res.send(result);
+//   /* After using Sequelize */
+//   models.Visitor.findOne({
+//     where: { id: req.query.id },
+//   }).then((result) => {
+//     console.log("findOne >>", result);
+//     res.send(result);
+//   });
+// };
+
+/* async, await */
+exports.getVisitor = async (req, res) => {
+  const result = await models.Visitor.findOne({
+    where: { id: req.query.id },
   });
+  // console.log(result);
+  res.send(result);
 };
 
-// PATCH /visitor/edit
-exports.patchVisitor = (req, res) => {
-  console.log(req.query);
+// PATCH /visitor/edit ///////////////////////////////////
+// /* .then() */
+// exports.patchVisitor = (req, res) => {
+//   // /* Before using Sequelize */
+//   // Visitor.patchVisitor(req.body, () => {
+//   //   res.send("Edit Succeed!");
+//   // }); // UPDATE visitor SET name = "${data.name}", comment = "${data.comment}" WHERE id = ${data.id};
 
-  // Send all request data to model, becuase don't know which data will change
-  Visitor.patchVisitor(req.body, () => {
-    res.send("Edit Succeed!");
-  });
+//   /* After using Sequelize */
+//   models.Visitor.update(
+//     {
+//       name: req.body.name,
+//       comment: req.body.comment,
+//     },
+//     {
+//       where: {
+//         id: req.body.id,
+//       },
+//     }
+//   ).then(() => {
+//     res.send("Edit Succeed!");
+//   });
+// };
+
+/* async, await */
+exports.patchVisitor = async (req, res) => {
+  const result = await models.Visitor.update(
+    {
+      name: req.body.name,
+      comment: req.body.comment,
+    },
+    {
+      where: {
+        id: req.body.id,
+      },
+    }
+  );
+  // console.log(result);
+  res.send("Edit Succeed!");
 };
 
-// DELTE /visitor/delete
-exports.deleteVisitor = (req, res) => {
-  console.log(req.body); // { id: n }
+// DELTE /visitor/delete /////////////////////////////////
+// /* .then() */
+// exports.deleteVisitor = (req, res) => {
+//   // /* Before using Sequelize */
+//   // Visitor.deleteVisitor(req.body.id, (result) => {
+//   //   res.send({ id: req.body.id });
+//   // }); // DELETE FROM visitor WHERE id = ${id};
+//   /* After using Sequelize */
+//   models.Visitor.destroy({
+//     where: {
+//       id: req.body.id,
+//     },
+//   }).then(() => {
+//     res.end();
+//   });
+// };
 
-  Visitor.deleteVisitor(req.body.id, (result) => {
-    // console.log("Cvisitor.js >>", result);
-    res.send({ id: req.body.id });
+/* async, await */
+exports.deleteVisitor = async (req, res) => {
+  const result = await models.Visitor.destroy({
+    where: { id: req.body.id },
   });
+  // console.log(result);
+  res.end();
 };
--- BE/10_Sequelize_withMVC/model/Visitor.js
+++ BE/10_Sequelize_withMVC/model/Visitor.js
@@ -1,10 +1,4 @@
-/* Before Connecting to MySQL */
-// exports.getVisitors = () => {
-//   return [
-//     { id: 1, name: "홍길동", comment: "내가 왔다." },
-//     { id: 2, name: "이찬혁", comment: "으라차차" },
-//   ];
-// };
+/* Before using Sequelize */
 
 /* Connected to MySQL */
 const mysql = require("mysql");
